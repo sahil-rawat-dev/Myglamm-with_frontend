aed1a3166ac8d3414e91ffa5145e6307c7275c0b
sahil-rawat-dev <sahilrawat2710@gmail.com>
2022-04-01 09:11:25 +0530

    frontend file

---
+++ components/index.deta.js
@@ -0,0 +1,118 @@
+let catagory = [
+  "https://files.myglamm.com/site-images/original/SKin-1-2.png",
+  "https://files.myglamm.com/site-images/original/Lips-3.png",
+  "https://files.myglamm.com/site-images/original/Superfoods-Model-120-X-120-px_2.jpg",
+  "https://files.myglamm.com/site-images/original/Eyes-4.png",
+  "https://files.myglamm.com/site-images/original/Face-5.png",
+  "https://files.myglamm.com/site-images/original/Nails-1.png",
+  "https://files.myglamm.com/site-images/original/App-PersonalCare_2.png",
+  "https://files.myglamm.com/site-images/original/Bath.png",
+];
+
+let spotlight=[
+    {
+        image:"https://files.myglamm.com/site-images/original/SF-Banner---700-x-488.jpg",
+        text1:"Superfood Summer Sale",
+        text2:"Superfood Summer Sale"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Summer-Hits-700x488.jpg",
+      text1:"Save Big on Combo",
+      text2:"Save Big on Combo"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/new-launches-700-x-488.jpg",
+      text1:"MyGlamm New Launches",
+      text2:"MyGlamm New Launches"
+    }
+]
+let launches=[
+  {
+      image:"https://files.myglamm.com/site-images/original/700x488-In-the-spotlight-Desktop_1.jpg",
+      text1:"THE MATTEST MATTE LIQUID LIPISTICK",
+      text2:"THE MATTEST MATTE LIQUID LIPISTICK"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Color-Pop-Pout-Homepage-banner-desktop-GIF--700-x-488_1.gif",
+      text1:"Superfoods Color Pop Lip balm",
+      text2:"Superfoods Color Pop Lip balm"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Nail-Enamel-In-the-spotlight-Desktop-700-x-488.jpg",
+      text1:"Two of Your Kind Nail Enamel",
+      text2:"Two of Your Kind Nail Enamel"
+    },
+]
+
+let essentials=[
+  {
+      image:"https://files.myglamm.com/site-images/original/Superfood-Collection-Banner-700-x-488.gif",
+      text1:"MyGlamm Superfood",
+      text2:"MyGlamm Superfood"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Glamm-Picks-700x488.jpg",
+      text1:"Under 499 Store",
+      text2:"Under 499 Store"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Glamm-Must-haves-desktop.jpg",
+      text1:"Under 999 Store",
+      text2:"Under 999 Store"
+    },
+]
+let bekitcombo = [
+  "https://files.myglamm.com/site-images/original/final-glow-SK-banner-700x488.gif",
+  "https://files.myglamm.com/site-images/original/Beauty-Calender-desktop-700x488.jpg",
+  "https://files.myglamm.com/site-images/original/Influencer-Store700x488.png",
+];
+
+let kitcombo=[
+  {
+      image:"https://files.myglamm.com/site-images/original/desktop-mk-combo.jpg",
+      text1:"Makeup Combos",
+      text2:"MyGlamm Superfood"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/desktop-skin-combo.jpg",
+      text1:"Skincare Combos",
+      text2:"Skincare Combos"
+    },
+    {
+      image:"https://files.myglamm.com/site-images/original/Desktop-combo.jpg",
+      text1:"Beaut Kits Under 499",
+      text2:"Beaut Kits Under 499"
+    },
+]
+
+let afkitcombo=[
+  "https://files.myglamm.com/site-images/original/Virtual-Try-On_12.jpg",
+  "https://files.myglamm.com/site-images/original/banner@2x-3_2.jpg"
+]
+
+let videoupperdata=[
+{
+  h2:"BRAND STORY",
+  p:"Backed by one of Europe's largest natural beauty companies, MyGlamm collaborated with global experts and makeup artists to bring about exciting innovation in makeup to accomplish our single, focused goal – make looking glamorous effortless!",
+  p1:"Know More",
+  image:"https://www.myglamm.com/images/ico-play.png"
+}
+]
+let offer=[
+  {
+    image:"https://files.myglamm.com/site-images/original/summerv-sale-2240x614_1.jpg",
+    text1:"Clearance Haul repeat",
+    text2:"Clearance Haul repeat"
+  },
+  {
+    image:"https://files.myglamm.com/site-images/original/freebie-2240x614.jpg",
+    text1:"Summer Secret MGXO",
+    text2:"Summer Secret MGXO"
+  },
+  {
+    image:"https://files.myglamm.com/site-images/original/mgxo-special-website.jpg",
+    text1:"MGXOSPECIAL new user",
+    text2:"MGXOSPECIAL new user"
+  }
+]
+export { catagory,spotlight,launches,essentials,bekitcombo,kitcombo,afkitcombo,videoupperdata,offer};
---
+++ scripts/index.js
@@ -0,0 +1,98 @@
+import { catagory,spotlight,launches,essentials,bekitcombo,kitcombo,afkitcombo,videoupperdata } from "../components/index.deta.js";
+import {appendimage,appenddata,showdata} from "./main.js";
+import data from "../deta/products.data.js";
+
+
+import {footer} from "../component/footer.js"
+//Code for adding catagory sections rounded-images
+catagory.forEach((el) => {
+    let boxdiv=document.getElementById("productbox-catagory")
+    let div=document.createElement("div")
+    let img = document.createElement("img");
+    img.src = el;
+    div.append(img)
+    boxdiv.append(div)
+    
+  });
+  let location=document.getElementById("spotligth")
+  appenddata(spotlight,location)
+
+//   append launches
+
+let location2=document.getElementById("launches")
+appenddata(launches,location2)
+
+// append essentials
+let location3=document.getElementById("essentials")
+appenddata(essentials,location3)
+
+
+let location4=document.getElementById("kit")
+appendimage(bekitcombo,location4)
+
+let location5=document.getElementById("kitscombos")
+appenddata(kitcombo,location5)
+
+let location6=document.getElementById("combos")
+appendimage(afkitcombo,location6)
+
+let bestseller = document.getElementById("product-parent");
+let bestsellerStart = 0;
+let bestsellerEnd = 3;
+function heading(str) {
+  let str2 = "";
+  for (let i = 0; i <= 12; i++) {
+    str2 = str2 + str[i];
+  }
+  str2 = str2 + "...";
+  return str2;
+}
+function addBestseller(start, end) {
+  bestseller.innerHTML = null;
+  for (let i = start; i <= end; i++) {
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    
+    let img = document.createElement("img");
+    img.src = data[i].displayimg;
+    card.appendChild(img);
+    let h2 = document.createElement("h3");
+    let title = "";
+    if (data[i].title.length >= 12) {
+      title = heading(data[i].title);
+    } else {
+      title = data[i].title;
+    }
+    h2.textContent = title;
+    card.appendChild(h2);
+    let p = document.createElement("p");
+    p.textContent = data[i].subtitle;
+    card.appendChild(p);
+    let price = document.createElement("p");
+    price.setAttribute("id", "price");
+    price.textContent = "₹ " + data[i].price;
+    card.appendChild(price);
+    card.addEventListener("click", () => {
+      localStorage.setItem("pdetails", JSON.stringify([data[i]]));
+      // window.location.href = "productdetails.html";
+      // console.log("ok");
+    });
+    bestseller.appendChild(card);
+  }
+}
+addBestseller(bestsellerStart, bestsellerEnd);
+setInterval(() => {
+  bestsellerStart++;
+  bestsellerEnd++;
+  if (bestsellerEnd == data.length) {
+    bestsellerStart = 0;
+    bestsellerEnd = 3;
+  }
+  addBestseller(bestsellerStart, bestsellerEnd);
+}, 3000);
+
+
+let footer1=document.getElementById("footer")
+footer1.innerHTML=footer();
+
+
